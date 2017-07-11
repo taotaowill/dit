85ef3a373b4523b7427a97a37f014b3509eb676f
taotaowill <taotaowill@gmail.com>
2017-07-11 09:07:15 +0800

    fix compile error

--- src/dit_flags.cc
+++ src/dit_flags.cc
@@ -1,7 +1,7 @@
 #include <gflags/gflags.h>
 
-DEFINE_string(nexus_addr, "/dit", "root path of dit regsiter on nexus");
-DEFINE_string(nexus_root, "127.0.0.1:8868", "server list of nexus");
+DEFINE_string(nexus_addr, "127.0.0.1:8868", "root path of dit regsiter on nexus");
+DEFINE_string(nexus_root, "/dit", "server list of nexus");
 DEFINE_string(server_port, "9999", "the rpc port of dit_server");
 DEFINE_string(server_nexus_prefix, "/server", "the dit server prefix on nexus");
 
