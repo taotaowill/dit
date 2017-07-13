0738d5887d482c64b8bc85be525404210976d59d
taotaowill <taotaowill@gmail.com>
2017-07-13 14:53:55 +0800

    modify build_dep.sh

--- build_deps.sh
+++ build_deps.sh
@@ -10,6 +10,8 @@ DEPS_SOURCE=$WORK_DIR/thirdsrc
 DEPS_PREFIX=$WORK_DIR/thirdparty
 DEPS_CONFIG="--prefix=${DEPS_PREFIX} --disable-shared --with-pic"
 FLAG_DIR=$WORK_DIR/.build
+GIT_REPO="http://gitlab.baidu.com/wanghaitao01/thirdsrc"
+#GIT_REPO="https://github.com/taotaowill/thirdsrc"
 
 export PATH=${DEPS_PREFIX}/bin:$PATH
 mkdir -p ${DEPS_SOURCE} ${DEPS_PREFIX} ${FLAG_DIR}
@@ -19,7 +21,7 @@ cd ${DEPS_SOURCE}
 # boost
 if [ ! -f "${FLAG_DIR}/boost_1_57_0" ] \
     || [ ! -d "${DEPS_PREFIX}/boost_1_57_0/boost" ]; then
-    wget -O boost_1_57_0.tar.gz --no-check-certificate https://jaist.dl.sourceforge.net/project/boost/boost/1.57.0/boost_1_57_0.tar.gz
+    wget --no-check-certificate -O boost_1_57_0.tar.gz  $GIT_REPO/raw/master/boost_1_57_0.tar.gz
     tar zxf boost_1_57_0.tar.gz
     rm -rf ${DEPS_PREFIX}/boost_1_57_0
     mv boost_1_57_0 ${DEPS_PREFIX}
@@ -32,9 +34,7 @@ fi
 if [ ! -f "${FLAG_DIR}/protobuf_2_6_1" ] \
     || [ ! -f "${DEPS_PREFIX}/lib/libprotobuf.a" ] \
     || [ ! -d "${DEPS_PREFIX}/include/google/protobuf" ]; then
-    rm -rf protobuf
-    git clone --depth=1 https://github.com/00k/protobuf
-    mv protobuf/protobuf-2.6.1.tar.gz .
+    wget --no-check-certificate -O protobuf-2.6.1.tar.gz $GIT_REPO/raw/master/protobuf-2.6.1.tar.gz
     tar zxf protobuf-2.6.1.tar.gz
     cd protobuf-2.6.1
     ./configure ${DEPS_CONFIG}
@@ -48,9 +48,7 @@ fi
 if [ ! -f "${FLAG_DIR}/snappy_1_1_1" ] \
     || [ ! -f "${DEPS_PREFIX}/lib/libsnappy.a" ] \
     || [ ! -f "${DEPS_PREFIX}/include/snappy.h" ]; then
-    rm -rf snappy
-    git clone --depth=1 https://github.com/00k/snappy
-    mv snappy/snappy-1.1.1.tar.gz .
+    wget --no-check-certificate -O snappy-1.1.1.tar.gz $GIT_REPO/raw/master/snappy-1.1.1.tar.gz
     tar zxf snappy-1.1.1.tar.gz
     cd snappy-1.1.1
     ./configure ${DEPS_CONFIG}
@@ -64,7 +62,7 @@ fi
 if [ ! -f "${FLAG_DIR}/sofa-pbrpc_1_1_3" ] \
     || [ ! -f "${DEPS_PREFIX}/lib/libsofa-pbrpc.a" ] \
     || [ ! -d "${DEPS_PREFIX}/include/sofa/pbrpc" ]; then
-    wget --no-check-certificate -O sofa-pbrpc-1.1.3.tar.gz https://github.com/baidu/sofa-pbrpc/archive/v1.1.3.tar.gz
+    wget --no-check-certificate -O sofa-pbrpc-1.1.3.tar.gz $GIT_REPO/raw/master/sofa-pbrpc-1.1.3.tar.gz
     tar zxf sofa-pbrpc-1.1.3.tar.gz
     cd sofa-pbrpc-1.1.3
     sed -i '/BOOST_HEADER_DIR=/ d' depends.mk
@@ -75,7 +73,6 @@ if [ ! -f "${FLAG_DIR}/sofa-pbrpc_1_1_3" ] \
     echo "SNAPPY_DIR=${DEPS_PREFIX}" >> depends.mk
     echo "PREFIX=${DEPS_PREFIX}" >> depends.mk
     cd src
-    #PROTOBUF_DIR=${DEPS_PREFIX} sh compile_proto.sh
     cd ..
     make -j4
     make install
@@ -85,7 +82,7 @@ fi
 
 # cmake for gflags
 if ! which cmake ; then
-    wget --no-check-certificate -O CMake-3.2.1.tar.gz https://github.com/Kitware/CMake/archive/v3.2.1.tar.gz
+    wget --no-check-certificate -O CMake-3.2.1.tar.gz $GIT_REPO/raw/master/CMake-3.2.1.tar.gz
     tar zxf CMake-3.2.1.tar.gz
     cd CMake-3.2.1
     ./configure --prefix=${DEPS_PREFIX}
@@ -98,7 +95,7 @@ fi
 if [ ! -f "${FLAG_DIR}/gflags_2_1_1" ] \
     || [ ! -f "${DEPS_PREFIX}/lib/libgflags.a" ] \
     || [ ! -d "${DEPS_PREFIX}/include/gflags" ]; then
-    wget --no-check-certificate -O gflags-2.1.1.tar.gz https://github.com/schuhschuh/gflags/archive/v2.1.1.tar.gz
+    wget --no-check-certificate -O gflags-2.1.1.tar.gz $GIT_REPO/raw/master/gflags-2.1.1.tar.gz
     tar zxf gflags-2.1.1.tar.gz
     cd gflags-2.1.1
     cmake -DCMAKE_INSTALL_PREFIX=${DEPS_PREFIX} -DGFLAGS_NAMESPACE=google -DCMAKE_CXX_FLAGS=-fPIC
@@ -112,7 +109,7 @@ fi
 if [ ! -f "${FLAG_DIR}/glog_0_3_3" ] \
     || [ ! -f "${DEPS_PREFIX}/lib/libglog.a" ] \
     || [ ! -d "${DEPS_PREFIX}/include/glog" ]; then
-    wget --no-check-certificate -O glog-0.3.3.tar.gz https://github.com/google/glog/archive/v0.3.3.tar.gz
+    wget --no-check-certificate -O glog-0.3.3.tar.gz $GIT_REPO/raw/master/glog-0.3.3.tar.gz
     tar zxf glog-0.3.3.tar.gz
     cd glog-0.3.3
     ./configure ${DEPS_CONFIG} CPPFLAGS=-I${DEPS_PREFIX}/include LDFLAGS=-L${DEPS_PREFIX}/lib
@@ -127,7 +124,7 @@ if [ ! -f "${FLAG_DIR}/gtest_1_7_0" ] \
     || [ ! -f "${DEPS_PREFIX}/lib/libgtest.a" ] \
     || [ ! -d "${DEPS_PREFIX}/include/gtest" ]; then
     rm -rf ./googletest-release-1.7.0
-    wget --no-check-certificate https://github.com/google/googletest/archive/release-1.7.0.tar.gz -O gtest_1_7_0.tar.gz
+    wget --no-check-certificate -O gtest_1_7_0.tar.gz $GIT_REPO/raw/master/gtest_1_7_0.tar.gz
     tar -xzf gtest_1_7_0.tar.gz
     cd googletest-release-1.7.0
     sed -i 's/-Wno-missing-field-initializers//g' cmake/internal_utils.cmake
@@ -143,7 +140,7 @@ fi
 if [ ! -f "${FLAG_DIR}/libunwind_0_99_beta" ] \
     || [ ! -f "${DEPS_PREFIX}/lib/libunwind.a" ] \
     || [ ! -f "${DEPS_PREFIX}/include/libunwind.h" ]; then
-    wget -O libunwind-0.99-beta.tar.gz http://nongnu.askapache.com/libunwind/libunwind-0.99-beta.tar.gz
+    wget --no-check-certificate -O libunwind-0.99-beta.tar.gz $GIT_REPO/raw/master/libunwind-0.99-beta.tar.gz
     tar zxf libunwind-0.99-beta.tar.gz
     cd libunwind-0.99-beta
     ./configure ${DEPS_CONFIG}
@@ -156,9 +153,7 @@ fi
 # gperftools (tcmalloc)
 if [ ! -f "${FLAG_DIR}/gperftools_2_2_1" ] \
     || [ ! -f "${DEPS_PREFIX}/lib/libtcmalloc_minimal.a" ]; then
-    rm -rf gperftools
-    git clone --depth=1 https://github.com/00k/gperftools
-    mv gperftools/gperftools-2.2.1.tar.gz .
+    wget --no-check-certificate -O gperftools-2.2.1.tar.gz $GIT_REPO/raw/master/gperftools-2.2.1.tar.gz
     tar zxf gperftools-2.2.1.tar.gz
     cd gperftools-2.2.1
     ./configure ${DEPS_CONFIG} CPPFLAGS=-I${DEPS_PREFIX}/include LDFLAGS=-L${DEPS_PREFIX}/lib
